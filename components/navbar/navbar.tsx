c1608bdc2096529c1ba6faf73ab13e78756fb369
Nick <bartolome_canitrot@terapiamia.com>
2022-08-13 14:45:54 -0300

    Main content app

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,15 @@
+
+import Image from 'next/image'
+
+
+
+const Navbar = () => {
+    return (
+        
+        <div className='navbar'>
+            
+        </div>
+    )
+}
+
+export default Navbar;
