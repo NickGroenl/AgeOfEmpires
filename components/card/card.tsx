d1864cad46f32f97503faebbfb7fe190bcd4ca10
Nick <bartolome_canitrot@terapiamia.com>
2022-08-14 14:07:48 -0300

    Civilization props

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,14 +1,16 @@
 
-import React, {FunctionComponent, useState} from 'react';
-import Image from 'next/image';
+import React, {FunctionComponent} from 'react';
 import styles from  '../../styles/Card.module.css';
 import { Civilizations } from '../../resources/types';
+import Link from 'next/link';
 
-const Card:FunctionComponent<Civilizations> = ({name, id}) => {
+const Card:FunctionComponent<Civilizations> = ({name, id,}) => {
     return (
-        <div className={styles.card}>
-            <h1 className={styles.title}>{name}</h1>
-        </div>
+        <Link className={styles.card} href={{ pathname: '/civs', query: {id} }}>
+            <h1 className={styles.title}>
+                {name}
+            </h1> 
+        </Link>
     )
 }
 
